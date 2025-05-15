0f360843634d05f9e2ec87659c50911bfc959766
abdulkareem67 <abdulkareemshahzad947@gmail.com>
2025-05-15 15:26:41 +0500

    Update LCA (2).cpp

--- LCA (2).cpp
+++ LCA (2).cpp
@@ -13,17 +13,7 @@ public:
         left = right = nullptr;
     }
 };
-// void printTree(TreeNode* root, int space = 0, int COUNT = 5) //Prints Tree Format
-// {
-//     if (root == nullptr) 
-//     return;
-//     space += COUNT;
-//     printTree(root->right, space);
-//     cout << endl;
-//     for (int i = COUNT; i < space; i++) cout << " ";
-//     cout << root->val << endl;
-//     printTree(root->left, space);
-// }
+
 TreeNode* findLCA(TreeNode* root, TreeNode* p, TreeNode* q) // LCA Calculating Function
 {
     if (!root || root == p || root == q) return root;
@@ -40,10 +30,6 @@ int main() {
     root->left->right = new TreeNode(5);
     root->right->left = new TreeNode(6);
     root->right->right = new TreeNode(7);
-
-    // // Print the tree in top-down format
-    // cout << "Tree structure:" << endl;
-    // printTree(root);
 
     TreeNode* p = root->left->left;
     TreeNode* q = root->left->right;
